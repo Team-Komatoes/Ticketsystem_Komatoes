278f892cddf2de3eacde62352e0bdeb9c730a86f
Roland D. <Delsing.roland@web.de>
2024-10-18 09:44:05 +0200

    zurück in die Hauptdatei

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template, jsonify  # pip install flask
 import mysql.connector
 from mysql.connector import Error
-from dbtest import test_db_connection, get_db_connection  # funktionen ausgelagert!
 
 app = Flask(__name__)
+
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="proj_koma",
+    )
 
 
 @app.route("/")
@@ -12,9 +20,18 @@ def home():
 
 
 @app.route("/test-db")
-def test_db():
-    result = test_db_connection
-    return jsonify(result)
+def test_db_connection():
+    try:
+        conn = get_db_connection()
+        if conn.is_connected():
+            return jsonify(
+                {"status": "success", "message": "Database connection succssesfull"}
+            )
+    except Error as e:
+        return jsonify({"status": "error", "message": str(e)})
+    finally:
+        if conn and conn.is_connected():
+            conn.close()
 
 
 if __name__ == "__main__":
