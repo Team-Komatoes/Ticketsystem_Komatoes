0ec439ec6196e0044e774af01433666feb79462b
Roland D. <Delsing.roland@web.de>
2024-11-11 15:44:05 +0100

    Priofeld einbau versuch

--- Frontend/app.py
+++ Frontend/app.py
@@ -5,8 +5,8 @@ from flask import (
     request,
     redirect,
 )  # pip install flask
-import mysql.connector                          #pip install mysql-connector-python
-import time                             
+import mysql.connector  # pip install mysql-connector-python
+import time
 from mysql.connector import Error
 
 app = Flask(__name__)
@@ -53,27 +53,42 @@ def get_Errors():
             conn.close()
 
 
+def get_prios():
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT Prio FROM priorität")
+        prio = cursor.fetchall()
+        print(prio)
+        return prio
+    except Error as e:
+        print(e)
+        return []
+    finally:
+        if conn and conn.is_connected():
+            conn.close()
+
+
+get_prios()
+
+
 @app.route("/")
 def home():
     stations = get_abteilungen()
     errors = get_Errors()
-    print("Errors: ", errors)
+
     return render_template("index.html", stations=stations, errors=errors)
 
 
-@app.route("/test-db")
-def test_db_connection():
-    try:
-        conn = get_db_connection()
-        if conn.is_connected():
-            return jsonify(
-                {"status": "success", "message": "Database connection succssesfull"}
-            )
-    except Error as e:
-        return jsonify({"status": "error", "message": str(e)})
-    finally:
-        if conn and conn.is_connected():
-            conn.close()
+@app.route("/get_option_data/<selected_option>")
+def get_option_data(selected_option):
+    data = get_prios()
+
+    for option in data:
+        if option[0] == selected_option:
+            return jsonify(content=option[0])
+
+    return jsonify(content="Keine Daten gefunden.")
 
 
 @app.route("/submit-ticket", methods=["POST"])
@@ -82,15 +97,14 @@ def submit():
     station_id = request.form["station"]
     fehler_id = request.form["fehler"]
     ticket_inhalt = request.form["ticketfeld"]
-    
-
+    prio = request.form["prio"]
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
         cursor.execute(
-            """ Insert INTO koma (Mitarbeiter, Abteilung, Fehler, Ticket)
-            VALUES (%s, %s, %s, %s)""",
-            (name, station_id, fehler_id, ticket_inhalt),
+            """ Insert INTO koma (Mitarbeiter, Abteilung, Fehler, Ticket, Prio)
+            VALUES (%s, %s, %s, %s, %s)""",
+            (name, station_id, fehler_id, ticket_inhalt, prio),
         )
         time.sleep(5)
         conn.commit()
@@ -102,7 +116,7 @@ def submit():
             cursor.close()
             conn.close()
 
-    return redirect ("/")
+    return redirect("/")
 
 
 if __name__ == "__main__":
